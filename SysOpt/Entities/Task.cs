45940b54955243d2834349e818b7ac3de5839787
Janusz Jagiello <jagi@netcompany.com>
2022-09-27 10:14:23 +0200

    Basic entities

---
+++ SysOpt/Entities/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysOpt
+{
+    internal class Task
+    {
+        public int Duration { get; set; }
+        public int Priority { get; set; }
+        public int Deadline { get; set; }
+        public string Name { get; set; }
+    }
+}
