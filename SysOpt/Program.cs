a6da063bf4f6bbcbb108b699b5d4d4bfe519c792
Fede Rico <frederikbroth@gmail.com>
2022-09-27 10:48:05 +0200

    Added Test cases

--- SysOpt/Program.cs
+++ SysOpt/Program.cs
@@ -6,6 +6,7 @@ using System.Globalization;
 
 static void LoadTasks(string path)
 {
+    
     using var reader = new StreamReader(path);
     var config = new CsvConfiguration(CultureInfo.InvariantCulture)
     {
@@ -20,18 +21,20 @@ static void LoadTasks(string path)
         string type = csv.GetField("type");
         if (type == "TT")
         {
-           //Create TT
+            //Create TT
+            Console.WriteLine("TT");
         }
         else if (type == "ET")
         {
-           //Create ET
+            Console.WriteLine("ET");
+
+            //Create ET
         }
 
 
     }
 }
-
+LoadTasks("test_cases\\inf_10_10\\taskset__1643188013-a_0.1-b_0.1-n_30-m_20-d_unif-p_2000-q_4000-g_1000-t_5__0__tsk.csv");
 Console.WriteLine("Hello, World!");
 
-var t = new MyTask();
 
