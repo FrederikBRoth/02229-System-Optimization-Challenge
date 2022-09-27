927f59134c984c0748e806662a5568b2135686fd
Fede Rico <frederikbroth@gmail.com>
2022-09-27 10:24:10 +0200

    Added csv parser

--- SysOpt/Program.cs
+++ SysOpt/Program.cs
@@ -1,3 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CsvHelper.Configuration;
+using CsvHelper;
+using SysOpt;
+using System.Globalization;
+
 Console.WriteLine("Hello, World!");
+
+static void LoadTasks(string path)
+{
+    using var reader = new StreamReader(path);
+    var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+    {
+        Delimiter = ";"
+    };
+    using var csv = new CsvReader(reader, config);
+
+    csv.Read();
+    csv.ReadHeader();
+    while (csv.Read())
+    {
+        string type = csv.GetField("type");
+        if (type == "TT")
+        {
+           //Create TT
+        }
+        else if (type == "ET")
+        {
+           //Create ET
+        }
+
+
+    }
+}
 
