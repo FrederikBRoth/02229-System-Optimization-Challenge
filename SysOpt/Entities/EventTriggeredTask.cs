10d286584b0c3832d7f8cae8e85ec1e4964c804d
Frederik Bøttger-Roth <frederikbroth@gmail.com>
2022-10-10 20:38:20 +0200

    Commented out multiple polling servers for now

--- SysOpt/Entities/EventTriggeredTask.cs
+++ SysOpt/Entities/EventTriggeredTask.cs
@@ -9,10 +9,10 @@ namespace SysOpt
     
     internal class EventTriggeredTask : MyTask
     {
-        public EventTriggeredTask(int minimalInterArrival, int duration, int priority, int deadline, string name, int seperation) : base(duration, priority, deadline, name)
+        public EventTriggeredTask(int minimalInterArrival, int duration, int priority, int deadline, string name) : base(duration, priority, deadline, name)
         {
             MinimalInterArrival = minimalInterArrival;
-            Seperation = seperation;
+            Seperation = 0;
         }
         public int MinimalInterArrival { get; set; }
         public int Seperation { get; set; }
