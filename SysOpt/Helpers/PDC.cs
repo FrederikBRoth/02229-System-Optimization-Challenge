f3028b045437b340167d4f0e4fe7ef404f478e57
marcus pedersen <marcus2860@gmail.com>
2022-11-15 11:31:05 +0100

    Completed PDC function and completed Unit Tests

--- SysOpt/Helpers/PDC.cs
+++ SysOpt/Helpers/PDC.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SysOpt.Helpers
 {
-    internal static class PDC
+    public static class PDC
     {
         public static bool IsSchedulableByPDC(int t1, int t2, List<TimeTriggeredTask> tasks)
         {
@@ -14,10 +14,13 @@ namespace SysOpt.Helpers
             foreach (TimeTriggeredTask task in tasks){
                 CompTimeSum += GetPDCompTime(t1, t2, task);
             }
+            if(CompTimeSum <= t2)
+                return true;
             return false;
         }
 
         //To be run on all tasks to calculate their process demand between t1 and t2
+        // It is expected that [0,L] is used for the time interval, L being LCM of tasks
         public static int GetPDCompTime(int t1, int t2, TimeTriggeredTask task)
         {
             // Sort out half complete periods
--- Unit Tests/UnitTests.cs
+++ Unit Tests/UnitTests.cs
@@ -10,6 +10,8 @@ namespace Unit_Tests
     public class UnitTests
     {
         string TTTestPath = "TestingCSV\\TTTest.csv";
+        string TTTest100 = "TestingCSV\\TTTest100Booked.csv";
+        string TTTestOB = "TestingCSV\\TTTestOverBooked.csv";
 
 
         /* Auxiliary Helper Methods */
@@ -90,6 +92,36 @@ namespace Unit_Tests
             expected0TickJobs = null;
             // Assert
             Assert.AreEqual(expected0TickJobs, EDFsimulation.GetReadyJobs(tasks.ttList, tick));
+        }
+
+        [TestMethod]
+        public void GetPDCompTime_Test()
+        {
+            // Arrange
+            TimeTriggeredTask task = new TimeTriggeredTask(5, 2, 7, 5, "TTTestTask");
+            int t1 = 0;
+            int t2 = 15;
+
+            // Assert
+            Assert.AreEqual(6, PDC.GetPDCompTime(t1, t2, task));
+        }
+
+        [TestMethod]
+        public void IsSchedulableByPDC_Test()
+        {
+            // Arrange
+            (List<TimeTriggeredTask> ttList, List<EventTriggeredTask> etList) tasks = TaskReader.LoadTasks(TTTestPath);
+            (List<TimeTriggeredTask> ttList, List<EventTriggeredTask> etList) tasks100 = TaskReader.LoadTasks(TTTest100);
+            (List<TimeTriggeredTask> ttList, List<EventTriggeredTask> etList) tasksOB = TaskReader.LoadTasks(TTTestOB);
+
+            int taskslcm = AuxiliaryHelper.GetLCM(tasks.ttList.Select(t => t.Period).ToArray());
+            int taskslcm100 = AuxiliaryHelper.GetLCM(tasks100.ttList.Select(t => t.Period).ToArray());
+            int taskslcmOB = AuxiliaryHelper.GetLCM(tasksOB.ttList.Select(t => t.Period).ToArray());
+
+            // Assert
+            Assert.IsTrue(PDC.IsSchedulableByPDC(0, taskslcm, tasks.ttList));
+            Assert.IsTrue(PDC.IsSchedulableByPDC(0, taskslcm100, tasks100.ttList));
+            Assert.IsFalse(PDC.IsSchedulableByPDC(0, taskslcmOB, tasksOB.ttList));
         }
     }
 }
